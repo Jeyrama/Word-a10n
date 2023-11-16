059ba5fee3f9f72d9dbbc9a04022a039506287ee
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-15 19:34:18 -0800

    Update Word-a10n.js

--- Word-a10n.js
+++ Word-a10n.js
@@ -29,3 +29,10 @@ Example:
 */
 
 // Solution
+
+let find = /[a-z]{4,}/gi;
+function replace(match) {return match[0] + (match.length - 2) + match[match.length - 1];}
+
+function abbreviate(string) {
+  return string.replace(find, replace);
+}
